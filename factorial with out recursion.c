89f8839d61a14807985901c75945f6617d30bec2
munibalaji155 <113886937+munibalaji155@users.noreply.github.com>
2022-10-06 13:24:58 +0530

    output

---
+++ factorial with out recursion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+   int n,i,j=1;
+   printf("Enter  the value of n:");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++){
+     j=j*i;
+   }
+   printf("factorial for %d:%d",n,j);
+}
+
+
